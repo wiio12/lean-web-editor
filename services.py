1b27a8846252340b26b560484be4c2f29ffdefb7
wanghaiming <wanghm39@mail2.sysu.edu.cn>
2023-11-29 15:59:38 +0800

    update apps

--- services.py
+++ services.py
@@ -8,6 +8,9 @@ app = Flask(__name__, static_folder='dist', static_url_path='')
 def index():
     return send_file('dist/index.html')
 
+
+
+# informal statement to informal solution
 @app.post('/api/problem_gpt-3.5__solve')
 def problem_gpt35_solve():
     content = request.json
@@ -16,48 +19,106 @@ def problem_gpt35_solve():
     print(f'FsToIs: {data}')
     return jsonify({'out': data})
 
-@app.post('/api/FsToIs')
-def FsToIs():
+@app.post('/api/problem_gpt-4__solve')
+def problem_gpt4_solve():
     content = request.json
-    data = content['data']
+    data = content['problem_data']
+    time.sleep(10)
     print(f'FsToIs: {data}')
     return jsonify({'out': data})
 
-@app.post('/api/IsToFs')
-def IsToFs():
+@app.post('/api/problem_llama2-7b__solve')
+def problem_llama27b_solve():
     content = request.json
-    data = content['data']
-    print(f'IsToFs: {data}')
-    return jsonify({'out': data})
-    
-@app.post('/api/IpToFp')
-def IpToFp():
-    content = request.json
-    data = content['data']
-    print(f'IpToFp: {data}')
-    return jsonify({'out': data})
-
-@app.post('/api/IsToIp')
-def IsToIp():
-    content = request.json
-    data = content['data']
-    print(f'IsToIp: {data}')
+    data = content['problem_data']
+    time.sleep(10)
+    print(f'FsToIs: {data}')
     return jsonify({'out': data})
 
 
-@app.post('/api/FsToFp')
-def FsToFp():
+
+
+# informal statement to formal statement
+@app.post('/api/problem_gpt-3.5__formalize')
+def problem_gpt35_formalize():
     content = request.json
-    data = content['data']
-    print(f'FsToFp: {data}')
+    data = content['problem_data']
+    time.sleep(10)
+    print(f'FsToIs: {data}')
     return jsonify({'out': data})
 
-@app.post('/api/FpToIp')
-def FpToIp():
+@app.post('/api/problem_gpt-4__formalize')
+def problem_gpt4_formalize():
     content = request.json
-    data = content['data']
-    print(f'FpToIp: {data}')
+    data = content['problem_data']
+    time.sleep(10)
+    print(f'FsToIs: {data}')
     return jsonify({'out': data})
+
+@app.post('/api/problem_llama2-7b__formalize')
+def problem_llama27b_formalize():
+    content = request.json
+    data = content['problem_data']
+    time.sleep(10)
+    print(f'FsToIs: {data}')
+    return jsonify({'out': data})
+
+
+
+
+# informal solution to formal proof
+@app.post('/api/solution_gpt-3.5__formalize')
+def solution_gpt35_formalize():
+    content = request.json
+    data = content['problem_data']
+    time.sleep(10)
+    print(f'FsToIs: {data}')
+    return jsonify({'out': data})
+
+@app.post('/api/solution_gpt-4__formalize')
+def solution_gpt4_formalize():
+    content = request.json
+    data = content['problem_data']
+    time.sleep(10)
+    print(f'FsToIs: {data}')
+    return jsonify({'out': data})
+
+@app.post('/api/solution_llama2-7b__formalize')
+def solution_llama27b_formalize():
+    content = request.json
+    data = content['problem_data']
+    time.sleep(10)
+    print(f'FsToIs: {data}')
+    return jsonify({'out': data})
+
+
+
+
+# formal statement to formal proof
+@app.post('/api/formal_gpt-3.5__solve')
+def formal_gpt35_solve():
+    content = request.json
+    data = content['problem_data']
+    time.sleep(10)
+    print(f'FsToIs: {data}')
+    return jsonify({'out': data})
+
+@app.post('/api/formal_gpt-4__solve')
+def formal_gpt4_solve():
+    content = request.json
+    data = content['problem_data']
+    time.sleep(10)
+    print(f'FsToIs: {data}')
+    return jsonify({'out': data})
+
+@app.post('/api/formal_llama2-7b__solve')
+def formal_llama27b_solve():
+    content = request.json
+    data = content['problem_data']
+    time.sleep(10)
+    print(f'FsToIs: {data}')
+    return jsonify({'out': data})
+
 
 print("Server is running...")
 serve(app, host='0.0.0.0', port='8080')
